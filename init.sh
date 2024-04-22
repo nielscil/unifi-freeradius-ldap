9f34f6c723017cdefa56565ecefa51becb07351f
Niels Bouma <9073152+nielscil@users.noreply.github.com>
2024-04-22 10:58:16 +0200

    Update env replacement

--- init.sh
+++ init.sh
@@ -8,8 +8,9 @@ echo "$@"
 [ -z "$ACCESS_ALLOWED_CIDR" ] && echo "ACCESS_ALLOWED_CIDR env variable not defined! Exiting..." && exit 1
 [ -z "$BASE_DOMAIN" ] && echo "BASE_DOMAIN env variable not defined! Exiting..." && exit 1
 [ -z "$DOMAIN_EXTENSION" ] && echo "DOMAIN_EXTENSION env variable not defined! Exiting..." && exit 1
-[ -z "$GOOGLE_LDAP_PASSWORD" ] && echo "GOOGLE_LDAP_PASSWORD env variable not defined! Exiting..." && exit 1
-[ -z "$GOOGLE_LDAP_USERNAME" ] && echo "GOOGLE_LDAP_USERNAME env variable not defined! Exiting..." && exit 1
+[ -z "$LDAP_SERVER" ] && echo "LDAP_SERVER env variable not defined! Exiting..." && exit 1
+[ -z "$LDAP_PASSWORD" ] && echo "LDAP_PASSWORD env variable not defined! Exiting..." && exit 1
+[ -z "$LDAP_USERNAME" ] && echo "LDAP_USERNAME env variable not defined! Exiting..." && exit 1
 
 # replace all those env params in the file
 sed -i "s|ACCESS_ALLOWED_CIDR|$ACCESS_ALLOWED_CIDR|g" /etc/freeradius/clients.conf
@@ -18,8 +19,9 @@ sed -i "s|SHARED_SECRET|$SHARED_SECRET|g" /etc/freeradius/clients.conf
 sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/proxy.conf
 sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/proxy.conf
 
-sed -i "s|GOOGLE_LDAP_PASSWORD|$GOOGLE_LDAP_PASSWORD|g" /etc/freeradius/mods-available/ldap
-sed -i "s|GOOGLE_LDAP_USERNAME|$GOOGLE_LDAP_USERNAME|g" /etc/freeradius/mods-available/ldap
+sed -i "s|LDAP_SERVER|$LDAP_SERVER|g" /etc/freeradius/mods-available/ldap
+sed -i "s|LDAP_PASSWORD|$GOOGLE_LDAP_PASSWORD|g" /etc/freeradius/mods-available/ldap
+sed -i "s|LDAP_USERNAME|$GOOGLE_LDAP_USERNAME|g" /etc/freeradius/mods-available/ldap
 
 # add support to second level like: .com.br, .com.ar
 sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/mods-available/ldap
