a7210f7c511db5beb0709d64e4a466129859461a
Niels Bouma <9073152+nielscil@users.noreply.github.com>
2024-04-22 14:10:21 +0200

    updated some stuff

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM freeradius/freeradius-server:3.0.23
 
-COPY configs/clients.conf /etc/freeradius/clients.conf
-COPY configs/default /etc/freeradius/sites-available/default
-COPY configs/inner-tunnel /etc/freeradius/sites-available/inner-tunnel
-COPY configs/ldap /etc/freeradius/mods-available/ldap
-COPY configs/eap /etc/freeradius/mods-enabled/eap
-COPY configs/proxy.conf /etc/freeradius/proxy.conf
+COPY --chown=101:101 --chmod=0640 configs/clients.conf /etc/freeradius/clients.conf
+COPY --chown=101:101 --chmod=0640 configs/default /etc/freeradius/sites-available/default
+COPY --chown=101:101 --chmod=0640 configs/inner-tunnel /etc/freeradius/sites-available/inner-tunnel
+COPY --chown=101:101 --chmod=0640 configs/ldap /etc/freeradius/mods-available/ldap
+COPY --chown=101:101 --chmod=0640 configs/eap /etc/freeradius/mods-enabled/eap
+COPY --chown=101:101 --chmod=0640 configs/proxy.conf /etc/freeradius/proxy.conf
 COPY init.sh /usr/local/bin
 RUN chmod +x /usr/local/bin/init.sh
 RUN ln -s /etc/freeradius/mods-available/ldap /etc/freeradius/mods-enabled/ldap
--- init.sh
+++ init.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-set -e
-set -x
-echo "$@"
-
 # Check if all env parameters exist
 [ -z "$ACCESS_ALLOWED_CIDR" ] && echo "ACCESS_ALLOWED_CIDR env variable not defined! Exiting..." && exit 1
 [ -z "$BASE_DOMAIN" ] && echo "BASE_DOMAIN env variable not defined! Exiting..." && exit 1
-[ -z "$DOMAIN_EXTENSION" ] && echo "DOMAIN_EXTENSION env variable not defined! Exiting..." && exit 1
+[ -z "$BASE_DN" ] && echo "BASE_DN env variable not defined! Exiting..." && exit 1
 [ -z "$LDAP_SERVER" ] && echo "LDAP_SERVER env variable not defined! Exiting..." && exit 1
 [ -z "$LDAP_PASSWORD" ] && echo "LDAP_PASSWORD env variable not defined! Exiting..." && exit 1
 [ -z "$LDAP_USERNAME" ] && echo "LDAP_USERNAME env variable not defined! Exiting..." && exit 1
@@ -17,18 +13,11 @@ sed -i "s|ACCESS_ALLOWED_CIDR|$ACCESS_ALLOWED_CIDR|g" /etc/freeradius/clients.co
 sed -i "s|SHARED_SECRET|$SHARED_SECRET|g" /etc/freeradius/clients.conf
 
 sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/proxy.conf
-sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/proxy.conf
 
 sed -i "s|LDAP_SERVER|$LDAP_SERVER|g" /etc/freeradius/mods-available/ldap
 sed -i "s|LDAP_PASSWORD|$GOOGLE_LDAP_PASSWORD|g" /etc/freeradius/mods-available/ldap
 sed -i "s|LDAP_USERNAME|$GOOGLE_LDAP_USERNAME|g" /etc/freeradius/mods-available/ldap
-
-# add support to second level like: .com.br, .com.ar
-sed -i "s|BASE_DOMAIN|$BASE_DOMAIN|g" /etc/freeradius/mods-available/ldap
-if [[ ${DOMAIN_EXTENSION} =~ [.] ]]; then
-    DOMAIN_EXTENSION=$( echo $DOMAIN_EXTENSION | awk -F'.' '{print $1",dc="$2}' )
-fi
-sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/mods-available/ldap
+sed -i "s|BASE_DN|$BASE_DN|g" /etc/freeradius/mods-available/ldap
 
 # Handle the certs
 cp /certs/ldap-client.key /etc/freeradius/certs/ldap-client.key
